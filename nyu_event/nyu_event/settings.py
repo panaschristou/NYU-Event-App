644bb2fc107215a00527997063e2ff05d565a1b1
Yiyang Zhou <yiyang.zhou@mail.utoronto.ca>
2024-02-20 14:29:12 -0500

    backend init

--- nyu_event/nyu_event/settings.py
+++ nyu_event/nyu_event/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,8 @@ SECRET_KEY = 'django-insecure--1vz#!np5i3v)p_(cclo9t8a40^ufcl#!tdkfq$p@6-=)ck@gf
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['nyu-event.us-east-1.elasticbeanstalk.com', '127.0.0.1']
+
 
 
 # Application definition
@@ -76,7 +78,7 @@ WSGI_APPLICATION = 'nyu_event.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
