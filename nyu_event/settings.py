0a7bd9b9495f1a1d06a31c18a038d09adb78df7c
杨天凡 <ty2492@nyu.edu>
2024-02-24 14:42:29 -0500

    updates for eb

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -93,7 +93,7 @@ else:
                         'USER': 'postgres',
                         'PASSWORD': 'Ytf@0930',
                         'HOST': 'localhost',
-                        'PORT': '5432',
+                        'PORT': '5431',
                 }
     }
 
