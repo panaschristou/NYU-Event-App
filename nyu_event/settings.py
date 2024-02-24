903c7412011a8267bc63793abfe936087bbe74e4
杨天凡 <ty2492@nyu.edu>
2024-02-24 15:34:34 -0500

    updates for eb

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -87,15 +87,11 @@ WSGI_APPLICATION = 'nyu_event.wsgi.application'
 #     }
 # else:
 DATABASES = {
-                    'default': {
-                        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-                        'NAME': 'nyu_event',
-                        'USER': 'postgres',
-                        'PASSWORD': 'Ytf@0930',
-                        'HOST': 'localhost',
-                        'PORT': '5431',
-                }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
