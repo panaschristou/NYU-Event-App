f434fed36e9552351d57d8f6671c93c05f41733b
Wanzhao Cheng <wc1609@nyu.edu>
2024-02-25 18:51:28 -0500

    Redirect to index page after logging in

--- backend/views.py
+++ backend/views.py
@@ -36,7 +36,7 @@ def login_user(request):
 		user = authenticate(request, username=username, password=password)
 		if user is not None:
 			login(request, user)
-			return redirect('register')
+			return redirect('index')
 		else:
 			messages.success(request, ("There Was An Error Logging In, Try Again..."))	
 			return redirect('login')
