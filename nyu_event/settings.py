22639c70c4e69ac85ce90ea59a4965317088f5c9
杨天凡 <ty2492@nyu.edu>
2024-02-29 13:58:17 -0500

    switch to postgress

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -75,19 +75,9 @@ WSGI_APPLICATION = "nyu_event.wsgi.application"
 
 
 # Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-# if 'RDS_DB_NAME' in os.environ:
-#     DATABASES = {
-#         'default': {
-#             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#             'NAME': os.environ['RDS_DB_NAME'],
-#             'USER': os.environ['RDS_USERNAME'],
-#             'PASSWORD': os.environ['RDS_PASSWORD'],
-#             'HOST': os.environ['RDS_HOSTNAME'],
-#             'PORT': os.environ['RDS_PORT'],
-#         }
-#     }
-# else:
+# https://docs.djangoproject.com/en/5.0/ref/settings/#databases
+
+
 if "RDS_DB_NAME" in os.environ:
     DATABASES = {
         "default": {
