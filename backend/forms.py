5b470b62865e767a04ccb191c18c2f4449418400
siddharthcv77 <sc10143@nyu.edu>
2024-03-01 12:52:45 -0500

    Check for "nyu@edu" in email

--- backend/forms.py
+++ backend/forms.py
@@ -10,6 +10,12 @@ class UserRegistrationForm(UserCreationForm):
         fields = ['username','email', 'password1', 'password2']
         # fields = ['first_name', 'last_name', 'username', 'email', 'password1', 'password2']
 
+    def clean_email(self):
+        email = self.cleaned_data.get('email')
+        if not email.endswith("@nyu.edu"):
+            raise forms.ValidationError("Email must be from NYU domain (@nyu.edu)")
+        return email
+
     def save(self, commit=True):
         user = super(UserRegistrationForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
