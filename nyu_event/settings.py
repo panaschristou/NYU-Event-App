ee73cbb489fcf3c09d7c2a9205e9acecb3aea7e5
Siddharth Chandrasekaran Vaikuntam <144621775+siddharthcv77@users.noreply.github.com>
2024-03-01 16:23:38 -0500

    Update settings.py

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'verify_email.apps.VerifyEmailConfig',
 ]
 
 MIDDLEWARE = [
