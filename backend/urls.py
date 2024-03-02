3d3f134641f558adad04ce92d6081ffb805c2aed
Wanzhao Cheng <wc1609@nyu.edu>
2024-03-02 14:10:13 -0500

    resolved conflicts

--- backend/urls.py
+++ backend/urls.py
@@ -6,10 +6,7 @@ from . import views
 urlpatterns = [
     path("login", views.login_user, name="login"),
     path("register", views.register_user, name="register"),
-    path("events/<int:event_id>/", views.event_detail, name="event_detail"),
     path("activate/<uidb64>/<token>", views.activate, name="activate"),
-    path('login',views.login_user, name="login"),
-    path('register',views.register_user, name="register"),
     path('events/<int:event_id>/', views.event_detail, name='event_detail'),
     path('search/', views.search_results, name='search_results'),
     path('', views.index_with_categories_view, name="index"),
--- backend/views.py
+++ backend/views.py
@@ -18,11 +18,26 @@ def index(request):
     )  # Assuming you want the newest events first
     return render(request, "index.html", {"events": events})
 
-
 def event_detail(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
-    return render(request, "event_detail.html", {"event": event})
+    category = request.GET.get('category')
+    return render(request, 'event_detail.html', {'event': event, 'category': category})
 
+def search_results(request):
+    search_query = request.GET.get('search_events', '').strip() 
+    events = Event.objects.filter(title__icontains=search_query) if search_query else Event.objects.none()
+    return render(request, 'search_results.html', {'events': events})
+
+EVENT_CATEGORIES = ["Musical", "Original", "Play", "Drama", "Revival", "Comedy", "Puppets"]
+
+def index_with_categories_view(request):
+    categories = EVENT_CATEGORIES
+    events = Event.objects.all() 
+    return render(request, 'index.html', {'categories': categories, 'events': events})
+
+def events_by_category(request, category):
+    events = Event.objects.filter(category__icontains=category)
+    return render(request, 'events_by_category.html', {'events': events, 'current_category': category})
 
 # This code snippet is called once the verification link is accessed by the user, to confirm the validity of the link.
 def activate(request, uidb64, token):
