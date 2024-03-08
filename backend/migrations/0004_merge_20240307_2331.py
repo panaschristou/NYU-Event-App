6e3d3d14624908ab8fb1fcf8bdbaa6c195fbb9fa
Yiyang Zhou <yiyang.zhou@mail.utoronto.ca>
2024-03-07 23:34:58 -0500

    .

--- backend/migrations/0004_merge_20240307_2331.py
+++ backend/migrations/0004_merge_20240307_2331.py
@@ -6,9 +6,8 @@ from django.db import migrations
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('backend', '0003_remove_event_availability'),
-        ('backend', '0003_remove_userevent_liked'),
+        ("backend", "0003_remove_event_availability"),
+        ("backend", "0003_remove_userevent_liked"),
     ]
 
-    operations = [
-    ]
+    operations = []
