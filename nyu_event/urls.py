7cffddf80a9c664307359a5a619eedb88e415758
siddharthcv77 <sc10143@nyu.edu>
2024-03-08 15:45:14 -0500

    Implemented_reset_password

--- nyu_event/urls.py
+++ nyu_event/urls.py
@@ -17,8 +17,16 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("user/", include("backend.urls")),
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(), name="reset_password"),
+    path('reset_password_sent/',auth_views.PasswordResetDoneView.as_view(), name="password_reset_done"),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name="password_reset_confirm"),
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(), name="password_reset_complete"),
 ]
+
+# template_name="reset_password.html"
