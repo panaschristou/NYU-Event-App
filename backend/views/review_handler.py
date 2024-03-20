f5e07a6959beecd7ef72c86335bb06380767ed2d
Wanzhao Cheng <wc1609@nyu.edu>
2024-03-20 14:44:07 -0400

    style fix

--- backend/views/review_handler.py
+++ backend/views/review_handler.py
@@ -4,13 +4,14 @@ from django.contrib.auth.decorators import login_required
 from backend.models import Event, Review
 from django.shortcuts import get_object_or_404
 
+
 @login_required
 @require_POST
 def post_review(request, event_id):
     event = get_object_or_404(Event, pk=event_id)
     user = request.user
-    rating = request.POST.get('rating')
-    review_text = request.POST.get('review_text')
+    rating = request.POST.get("rating")
+    review_text = request.POST.get("review_text")
     review = Review(event=event, user=user, rating=rating, review_text=review_text)
     review.save()
-    return JsonResponse({'success': True, 'review_id': review.id})
+    return JsonResponse({"success": True, "review_id": review.id})
