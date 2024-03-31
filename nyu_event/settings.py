9ec8e08d4d9c5b8c734e3ea228f116984170eb39
杨天凡 <ty2492@nyu.edu>
2024-03-30 22:16:14 -0400

    fix

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -34,7 +34,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "127.0.0.1",
     "main2.us-east-1.elasticbeanstalk.com",
-    'postgres-dev2.us-east-1.elasticbeanstalk.com',
+    "postgres-dev2.us-east-1.elasticbeanstalk.com",
 ]
 
 
