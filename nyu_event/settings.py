86ddb5de868a7246f4d6dc149229de6a5ce2ef36
Yiyang Zhou <vial0520@gmail.com>
2024-04-05 18:19:12 -0400

    fix aws

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -32,6 +32,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = ['*','nyu-event-dev.us-east-1.elasticbeanstalk.com']
 ALLOWED_HOSTS = [
+    "*",
     "127.0.0.1",
     "main2.us-east-1.elasticbeanstalk.com",
     "postgres-dev2.us-east-1.elasticbeanstalk.com",
