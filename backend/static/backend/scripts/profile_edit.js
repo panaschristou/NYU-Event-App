3e026ac25a1232049473d2e2df77d7a56d493b06
Yiyang Zhou <vial0520@gmail.com>
2024-04-11 17:53:56 -0400

    cropper

--- backend/static/backend/scripts/profile_edit.js
+++ backend/static/backend/scripts/profile_edit.js
@@ -16,3 +16,25 @@ $(".delete_account")[0].addEventListener("click", () => {
     });
 });
 
+const avatarInput = document.getElementById("avatar-input");
+const imageBox = document.getElementById("image-box");
+avatarInput.addEventListener("change", () => {
+  console.log("change");
+  const imgFile = avatarInput.files[0];
+  const imgUrl = URL.createObjectURL(imgFile);
+  imageBox.innerHTML = `<img id="image-cropper" class="hidden" width="400px" src=${imgUrl} />`;
+
+  const image = document.getElementById("image-cropper");
+  const cropper = new Cropper(image, {
+    aspectRatio: 1 / 1,
+    crop(event) {
+      console.log(event.detail.x);
+      console.log(event.detail.y);
+      console.log(event.detail.width);
+      console.log(event.detail.height);
+      console.log(event.detail.rotate);
+      console.log(event.detail.scaleX);
+      console.log(event.detail.scaleY);
+    },
+  });
+});
