e968b5634478855c8a728225d80354988e1a727d
Yiyang Zhou <vial0520@gmail.com>
2024-04-11 20:05:08 -0400

    avatar clear button

--- backend/static/backend/scripts/profile_edit.js
+++ backend/static/backend/scripts/profile_edit.js
@@ -16,25 +16,38 @@ $(".delete_account")[0].addEventListener("click", () => {
     });
 });
 
+function removeListeners(element) {
+  const newElement = element.cloneNode(true);
+  element.parentNode.replaceChild(newElement, element);
+  return newElement;
+}
+
 const avatarInput = document.getElementById("avatar-input");
 const imageBox = document.getElementById("image-box");
+let avatarClear = document.getElementById("avatar-clear");
+const croppedImageData = document.getElementById("cropped-image-data");
 avatarInput.addEventListener("change", () => {
-  console.log("change");
   const imgFile = avatarInput.files[0];
   const imgUrl = URL.createObjectURL(imgFile);
-  imageBox.innerHTML = `<img id="image-cropper" class="hidden" width="400px" src=${imgUrl} />`;
+
+  imageBox.innerHTML = `<img id="image-cropper" class="hidden" src=${imgUrl} />`;
+  avatarClear.classList.remove("hidden");
+  avatarInput.classList.add("hidden");
 
   const image = document.getElementById("image-cropper");
   const cropper = new Cropper(image, {
     aspectRatio: 1 / 1,
-    crop(event) {
-      console.log(event.detail.x);
-      console.log(event.detail.y);
-      console.log(event.detail.width);
-      console.log(event.detail.height);
-      console.log(event.detail.rotate);
-      console.log(event.detail.scaleX);
-      console.log(event.detail.scaleY);
-    },
+  });
+
+  avatarClear = removeListeners(avatarClear);
+  avatarClear.addEventListener("click", (e) => {
+    console.log("clicked");
+    e.preventDefault();
+
+    imageBox.innerHTML = "";
+    avatarClear.classList.add("hidden");
+    avatarInput.classList.remove("hidden");
+
+    avatarInput.value = "";
   });
 });
