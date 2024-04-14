941c1d9c2d72185bd6806f1014ddd072b469fc33
Yiyang Zhou <vial0520@gmail.com>
2024-04-14 16:09:27 -0400

    fix aws

--- nyu_event/settings.py
+++ nyu_event/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure--1vz#!np5i3v)p_(cclo9t8a40^ufcl#!tdkfq$p@6-=)ck@gf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ["APP_ENV"] not in ["develop", "production"]
 
 # ALLOWED_HOSTS = ['*','nyu-event-dev.us-east-1.elasticbeanstalk.com']
 ALLOWED_HOSTS = [
