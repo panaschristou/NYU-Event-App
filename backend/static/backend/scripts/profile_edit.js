0f460b62de3b0c29fe9e9e86e0e69fa681988ae9
Yiyang Zhou <vial0520@gmail.com>
2024-04-14 20:51:14 -0400

    more sophisticated logic for avatar upload

--- backend/static/backend/scripts/profile_edit.js
+++ backend/static/backend/scripts/profile_edit.js
@@ -3,7 +3,9 @@ const avatarInput = document.getElementById("avatar-input");
 const imageBox = document.getElementById("image-box");
 let avatarClear = document.getElementById("avatar-clear");
 let avatarSave = document.getElementById("avatar-save");
+let formSubmit = $(".profile-form-submit")[0];
 const croppedImageData = document.getElementById("cropped-image-data");
+const avatarMessage = document.getElementById("avatar-message");
 
 $(".delete_account")[0].addEventListener("click", () => {
   fetch("/user/delete-account", {
@@ -48,17 +50,25 @@ avatarInput.addEventListener("change", () => {
   const cropper = new Cropper(image, {
     aspectRatio: 1 / 1,
   });
+  let savedCropper = null;
 
   avatarClear = removeListeners(avatarClear);
   avatarClear.addEventListener("click", (e) => {
     switchOffCropperUI();
-    avatarInput.value = "";
+    savedCropper = null;
+    avatarMessage.classList.add("hidden");
   });
 
   avatarSave = removeListeners(avatarSave);
   avatarSave.addEventListener("click", (e) => {
     switchOffCropperUI();
-    cropper.getCroppedCanvas().toBlob((blob) => {
+    savedCropper = cropper;
+    avatarMessage.classList.remove("hidden");
+  });
+
+  formSubmit = removeListeners(formSubmit);
+  formSubmit.addEventListener("click", (e) => {
+    savedCropper.getCroppedCanvas().toBlob((blob) => {
       const fd = new FormData();
       fd.append("file", blob);
       fetch(`avatar`, {
@@ -74,4 +84,7 @@ avatarInput.addEventListener("change", () => {
         });
     });
   });
+
+  // force avatarInput listener to catch change even when users are submitting the same file
+  avatarInput.value = null;
 });
