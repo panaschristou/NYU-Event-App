4287400731be98d8ab74aa7914ba0dfac4297df0
杨天凡 <ty2492@nyu.edu>
2024-04-18 00:13:27 -0400

    fix

--- backend/signals.py
+++ backend/signals.py
@@ -4,7 +4,7 @@ from django.dispatch import receiver
 from django.core.mail import send_mail
 from .models import Profile
 from django.conf import settings
-from .models import SuspendedUser
+from .models import SuspendedUser, BannedUser
 
 
 @receiver(post_save, sender=User)
@@ -22,7 +22,39 @@ def save_profile(sender, instance, **kwargs):
 def send_suspension_notification(sender, instance, **kwargs):
     if instance.is_suspended:
         subject = "Your account has been suspended"
-        message = f"Dear {instance.user.username},\n\nYour account has been suspended due to the following reason:\n\n{instance.reason}\n\nIf you believe this is an error, please contact support.\n\nSincerely,\nThe Admin Team"
+        unsuspend_time = (
+            instance.unsuspended_at.strftime("%Y-%m-%d %H:%M:%S")
+            if instance.unsuspended_at
+            else "unknown"
+        )
+        message = f"Dear {instance.user.username},\n\nYour account has been suspended until {unsuspend_time} due to the following reason:\n\n{instance.reason}\n\nIf you believe this is an error, please contact support.\n\nSincerely,\nThe Admin Team"
+        sender_email = settings.DEFAULT_FROM_EMAIL
+        recipient_email = instance.user.email
+        send_mail(subject, message, sender_email, [recipient_email])
+    else:
+        subject = "Your account has been unsuspended"
+        message = f"Dear {instance.user.username},\n\nYour account has been unsuspended \n\nSincerely,\nThe Admin Team"
+        sender_email = settings.DEFAULT_FROM_EMAIL
+        recipient_email = instance.user.email
+        send_mail(subject, message, sender_email, [recipient_email])
+
+
+@receiver(post_save, sender=BannedUser)
+def send_ban_notification(sender, instance, **kwargs):
+    if not instance.user.is_active:
+        subject = "Your account has been banned"
+        unban_time = (
+            instance.unban_at.strftime("%Y-%m-%d %H:%M:%S")
+            if instance.unban_at
+            else "unknown"
+        )
+        message = f"Dear {instance.user.username},\n\nYour account has been banned until {unban_time} due to the following reason:\n\n{instance.reason}\n\nIf you believe this is an error, please contact support.\n\nSincerely,\nThe Admin Team"
+        sender_email = settings.DEFAULT_FROM_EMAIL
+        recipient_email = instance.user.email
+        send_mail(subject, message, sender_email, [recipient_email])
+    else:
+        subject = "Your account has been unsuspended"
+        message = f"Dear {instance.user.username},\n\nYour account has been unbanned \n\nSincerely,\nThe Admin Team"
         sender_email = settings.DEFAULT_FROM_EMAIL
         recipient_email = instance.user.email
         send_mail(subject, message, sender_email, [recipient_email])
